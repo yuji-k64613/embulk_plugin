12de7c8d110a04640e4cbd6a7a6a485a72384bca
Your Name <you@example.com>
2016-06-05 12:38:44 +0900

    修正

--- src/main/java/org/embulk/filter/myfilter/MyfilterFilterPlugin.java
+++ src/main/java/org/embulk/filter/myfilter/MyfilterFilterPlugin.java
@@ -1,16 +1,8 @@
 package org.embulk.filter.myfilter;
 
 import com.google.common.base.Optional;
-import org.embulk.config.Config;
-import org.embulk.config.ConfigDefault;
-import org.embulk.config.ConfigDiff;
-import org.embulk.config.ConfigSource;
-import org.embulk.config.Task;
-import org.embulk.config.TaskSource;
-import org.embulk.spi.Column;
-import org.embulk.spi.FilterPlugin;
-import org.embulk.spi.PageOutput;
-import org.embulk.spi.Schema;
+import org.embulk.config.*;
+import org.embulk.spi.*;
 
 public class MyfilterFilterPlugin
         implements FilterPlugin
@@ -45,12 +37,91 @@ public class MyfilterFilterPlugin
     }
 
     @Override
-    public PageOutput open(TaskSource taskSource, Schema inputSchema,
-            Schema outputSchema, PageOutput output)
+    public PageOutput open(TaskSource taskSource, final Schema inputSchema,
+                           final Schema outputSchema, final PageOutput output)
     {
         PluginTask task = taskSource.loadTask(PluginTask.class);
 
         // Write your code here :)
-        throw new UnsupportedOperationException("MyfilterFilterPlugin.open method is not implemented yet");
+        //throw new UnsupportedOperationException("MyfilterFilterPlugin.open method is not implemented yet");
+
+        return new PageOutput() {
+            private final PageReader pageReader = new PageReader(inputSchema);
+            private final PageBuilder pageBuilder = new PageBuilder(Exec.getBufferAllocator(), outputSchema, output);
+
+            @Override
+            public void finish() {
+                pageBuilder.finish();
+            }
+
+            @Override
+            public void close() {
+                pageBuilder.close();
+            }
+
+            @Override
+            public void add(Page page) {
+                pageReader.setPage(page);
+
+                while (pageReader.nextRecord()) {
+                    inputSchema.visitColumns(new ColumnVisitor() {
+                        @Override
+                        public void booleanColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setBoolean(column, pageReader.getBoolean(column));
+                            }
+                        }
+
+                        @Override
+                        public void longColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setLong(column, pageReader.getLong(column));
+                            }
+                        }
+
+                        @Override
+                        public void doubleColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setDouble(column, pageReader.getDouble(column));
+                            }
+                        }
+
+                        @Override
+                        public void stringColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setString(column, pageReader.getString(column));
+                            }
+                        }
+
+                        @Override
+                        public void timestampColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setTimestamp(column, pageReader.getTimestamp(column));
+                            }
+                        }
+
+                        @Override
+                        public void jsonColumn(Column column) {
+                            if (pageReader.isNull(column)) {
+                                pageBuilder.setNull(column);
+                            } else {
+                                pageBuilder.setJson(column, pageReader.getJson(column));
+                            }
+                        }
+                    });
+                    pageBuilder.addRecord();
+                }
+            }
+        };
     }
 }
